848ada2b255156da5671d084ed2e58306fcfe998
이민제 <iminje@Mac.gordon.edu>
2026-04-27 15:17:42 -0400

    change on the border and background of technician page ui

--- my-app/src/app/technician/TechnicianDetail.tsx
+++ my-app/src/app/technician/TechnicianDetail.tsx
@@ -81,7 +81,7 @@ export default function TechnicianDetail({
 
   return (
     <>
-      <div className="border rounded-lg p-6 bg-white">
+      <div className="border border-gray-200 rounded-lg p-6 bg-white">
         <div className="mb-4">
           <div className="flex items-start justify-between mb-2">
             <div className="flex-1">
